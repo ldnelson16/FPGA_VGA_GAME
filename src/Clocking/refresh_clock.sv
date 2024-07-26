19a56b56acffbf8d58fcd691d8f3099d29a022ee
lukenels <ldnelson16@gmail.com>
2024-07-26 00:42:19 -0400

    clock signal modules

---
+++ src/Clocking/refresh_clock.sv
@@ -0,0 +1,16 @@
+`ifndef REFRESH_CLOCK
+`define REFRESH_CLOCK
+
+`include "../../parameters/clocking.sv"
+
+module refresh_clock(
+  input wire clk_50,
+  output refresh_clock,
+);
+  reg[21:0] refresh_counter; // counter
+  always @ (posedge MAX10_CLK1_50) begin
+    refresh_counter <= (refresh_counter == REFRESH_MAX) ? 0 : (refresh_counter + 1);
+  end; assign refresh_clock = (refresh_counter == REFRESH_MAX);
+endmodule
+
+`endif // REFRESH_CLOCK
