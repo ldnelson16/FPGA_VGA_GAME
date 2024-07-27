91cbcd1ed1d2e10d48283f6e6627d99e2566a4eb
lukenels <ldnelson16@gmail.com>
2024-07-27 00:40:49 -0400

    compiling on v1

--- src/VGA_driver/structures.sv
+++ src/VGA_driver/structures.sv
@@ -11,13 +11,13 @@ typedef struct {
 
 interface Frame();
   // variables
-  Pixel frame_data[`H_FRAME_HT-1: 0][`V_FRAME_HT-1: 0];
+  Pixel frame_data[`V_FRAME_HT-1: 0][`H_FRAME_HT-1: 0];
 
-  task update_pixel(int row, int col, Pixel pixel_in);
+  task update_pixel(input int row, input int col, input Pixel pixel_in);
     frame_data[row][col] = pixel_in;
   endtask
 
-  task get_pixel(int row, int col, Pixel pixel_out);
+  task get_pixel(input int row, input int col, output Pixel pixel_out);
     pixel_out = frame_data[row][col];
   endtask
 endinterface
--- src/main.sv
+++ src/main.sv
@@ -5,18 +5,20 @@
 `include "Clocking/VGA_clock.v"
 `include "Clocking/refresh_clock.sv"
 `include "VGA_driver/structures.sv"
+`include "Graphics/graphics_driver.sv"
 
 module main(
   input wire MAX10_CLK1_50, 
+  input wire rst, // must determine this input
   output reg[3:0] VGA_R, VGA_G, VGA_B, // RGB
   output reg VGA_HS, VGA_VS
 );
-  // // PLL for 147.14 MHz clock
-  // wire VGA_clk;
-  // VGA_clock VGA_CLOCK (
-  //   .inclk0(MAX10_CLK1_50),
-  //   .c0(VGA_clk)
-  // );
+  // PLL for 147.14 MHz clock
+  wire VGA_clk;
+  VGA_clock VGA_CLOCK (
+    .inclk0(MAX10_CLK1_50),
+    .c0(VGA_clk)
+  );
 
   // Counter for Refresh Rate 60 Hz clock
   wire refresh;
@@ -25,21 +27,30 @@ module main(
     .refresh_clock(refresh)
   );
 
-  // Frame frame_a();
-  // Frame frame_b();
+  Frame frame_a();
+  Frame frame_b();
 
-  // // vga controller
-  // vga_controller VGA_CONTROLLER (
-  //   .clk(VGA_clock),
-  //   .refresh(refresh),
-  //   .frame_a(frame_a),
-  //   .frame_b(frame_b),
-  //   .hsync(VGA_HS),
-  //   .vsync(VGA_VS),
-  //   .red({VGA_R[0], VGA_R[1], VGA_R[2], VGA_R[3]}),
-  //   .green({VGA_G[0], VGA_G[1], VGA_G[2], VGA_G[3]}),
-  //   .blue({VGA_B[0], VGA_B[1], VGA_B[2], VGA_B[3]})
-  // );
+  // graphics driver, determines what is in frame_a/b
+  graphics_driver GRAPHICS_DRIVER (
+    .clk(VGA_clock),
+    .refresh(refresh),
+    .rst(rst),
+    .frame_a(frame_a),
+    .frame_b(frame_b)
+  );
+
+  // vga controller
+  vga_controller VGA_CONTROLLER (
+    .clk(VGA_clock),
+    .refresh(refresh),
+    .frame_a(frame_a),
+    .frame_b(frame_b),
+    .hsync(VGA_HS),
+    .vsync(VGA_VS),
+    .red({VGA_R[0], VGA_R[1], VGA_R[2], VGA_R[3]}),
+    .green({VGA_G[0], VGA_G[1], VGA_G[2], VGA_G[3]}),
+    .blue({VGA_B[0], VGA_B[1], VGA_B[2], VGA_B[3]})
+  );
 endmodule 
 
 `endif // MAIN
