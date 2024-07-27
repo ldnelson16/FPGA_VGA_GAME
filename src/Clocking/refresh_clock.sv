9382835362454c7f8339d36a43b631113566e92b
ldnelson16 <ldnelson16@gmail.com>
2024-07-26 23:30:41 -0400

    PLL causes unexplained errors, like it literally doesn't explain them

--- src/Clocking/refresh_clock.sv
+++ src/Clocking/refresh_clock.sv
@@ -5,12 +5,13 @@
 
 module refresh_clock(
   input wire clk_50,
-  output refresh_clock,
+  output refresh_clock
 );
   reg[21:0] refresh_counter; // counter
-  always @ (posedge MAX10_CLK1_50) begin
-    refresh_counter <= (refresh_counter == REFRESH_MAX) ? 0 : (refresh_counter + 1);
-  end; assign refresh_clock = (refresh_counter == REFRESH_MAX);
+  always @ (posedge clk_50) begin
+    refresh_counter <= (refresh_counter == `REFRESH_MAX) ? 0 : (refresh_counter + 1);
+  end
+  assign refresh_clock = (refresh_counter == `REFRESH_MAX);
 endmodule
 
 `endif // REFRESH_CLOCK
