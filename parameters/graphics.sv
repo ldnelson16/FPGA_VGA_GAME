de22cfbb988e05ef70852400cc1bff3095add774
ldnelson16 <ldnelson16@gmail.com>
2024-07-28 21:35:14 -0400

    default parameters

---
+++ parameters/graphics.sv
@@ -0,0 +1,7 @@
+`ifndef GRAPHICS_PARAMETERS
+`define GRAPHICS_PARAMETERS
+
+`define GRASS_PERCENTAGE 30 // 30% of screen is grass
+`define STARTING_SPEED 1 // recommended: 1
+
+`endif // GRAPHICS_PARAMETERS
