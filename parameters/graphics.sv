adcecd4fbf257ea65c9e79798428ad7173574e5f
ldnelson16 <ldnelson16@gmail.com>
2024-08-04 21:33:45 -0400

    parameters for screen objects

--- parameters/graphics.sv
+++ parameters/graphics.sv
@@ -4,4 +4,7 @@
 `define GRASS_PERCENTAGE 30 // 30% of screen is grass
 `define STARTING_SPEED 1 // recommended: 1
 
+`define PLAYER_WIDTH 1 // num of display pixels, not monitor pixels
+`define PLAYER_HEIGHT 1
+
 `endif // GRAPHICS_PARAMETERS
